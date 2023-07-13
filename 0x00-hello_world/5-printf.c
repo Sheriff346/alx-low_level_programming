8bb6791beb9dab1873b368687b527e45cf7ea18e
Sheriff346 <afenusherif23@gmail.com>
2023-07-13 09:54:43 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that printf a line with puts function
+ * main - A program that prints a line with puts function
  * Return: 0 (Success)
  */
 int main(void)
 {
-puts("with proper grammar, but the outcome is a piece of art,\n")
-return (0)
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
 
